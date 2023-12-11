9309c73d8ec40e34317a57e5e7dfe74962792d4d
수연 <3437ssy@naver.com>
2023-12-11 19:07:23 +0900

    에러 처리 방법

--- src/main/java/com/cos/photogramstart/web/AuthController.java
+++ src/main/java/com/cos/photogramstart/web/AuthController.java
@@ -1,15 +1,29 @@
 package com.cos.photogramstart.web;
 
 import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomVaildationException;
+import com.cos.photogramstart.service.AuthService;
 import com.cos.photogramstart.web.dto.auth.SignupDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
 @Slf4j
 @Controller
 public class AuthController {
+
+    private final AuthService authService;
 
     @GetMapping("/auth/signin")
     public String signinForm() {
@@ -20,11 +34,28 @@ public class AuthController {
     public String signupForm() {
         return "/auth/signup";
     }
+
     @PostMapping("/auth/signup")
-    public String signup(SignupDto signupDto) {
-        log.info(signupDto.toString());
-        User user= signupDto.toEntity();
-        log.info(user.toString());
-        return "/auth/signin";
+    public String signup(@Valid SignupDto signupDto, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+                System.out.println(" = ====================================================================");
+                System.out.println("error.getDefaultMessage() = " + error.getDefaultMessage());
+
+            }
+            throw new CustomVaildationException("유효성검사 실패함",errorMap);
+        } else {
+            log.info(signupDto.toString());
+            User user = signupDto.toEntity();
+            log.info(user.toString());
+            User userEntity = authService.회원가입(user);
+            System.out.println("userEntity = " + userEntity);
+            return "/auth/signin";
+        }
+
     }
 }
