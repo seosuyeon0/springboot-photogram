67158a9e3b0fe344e345fb1d850d8a4c8a49e42d
수연 <3437ssy@naver.com>
2023-12-11 19:07:57 +0900

    로그인 시큐리티

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -3,11 +3,16 @@ package com.cos.photogramstart.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -17,6 +22,7 @@ public class SecurityConfig {
                 .and()
                 .formLogin()
                 .loginPage("/auth/signin")
+                .loginProcessingUrl("/auth/signin")
                 .defaultSuccessUrl("/");
         return http.build();
     }
