6c5028c81a590beff20079c10c88f30794c12baa
수연 <3437ssy@naver.com>
2023-12-12 22:42:39 +0900

    회원정보 변경

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -17,12 +18,8 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id,  @AuthenticationPrincipal PrincipalDetails principalDetails) {
-        // 1  AuthenticationPrincipal 어노테이션 사용방법 : 추천
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
         System.out.println("principalDetails.getUser() = " + principalDetails.getUser());
-        // 2. 어노테이션 없이 : 비추천
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("auth.getPrincipal() = " + auth.getPrincipal());
         return "user/update" ;
     }
 }
