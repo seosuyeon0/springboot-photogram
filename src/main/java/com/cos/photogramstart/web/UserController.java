d5570d0f8c3b8ea2c318db1fbcf710991ac71172
수연 <3437ssy@naver.com>
2023-12-12 16:41:23 +0900

    로그인 - AuthenticationPrincipal 사용

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +17,12 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id) {
-        return "user/update";
+    public String update(@PathVariable int id,  @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        // 1  AuthenticationPrincipal 어노테이션 사용방법 : 추천
+        System.out.println("principalDetails.getUser() = " + principalDetails.getUser());
+        // 2. 어노테이션 없이 : 비추천
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("auth.getPrincipal() = " + auth.getPrincipal());
+        return "user/update" ;
     }
 }
