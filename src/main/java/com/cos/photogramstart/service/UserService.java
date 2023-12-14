c8a33bcb63a965deaf9fb480899a2fa8f9201105
수연 <3437ssy@naver.com>
2023-12-14 21:34:56 +0900

    회원정보 변경

---
+++ src/main/java/com/cos/photogramstart/service/UserService.java
@@ -0,0 +1,36 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Transactional
+    public User 회원수정(int id, User user) {
+        User userEntity = userRepository.findById(id).get();
+
+        userEntity.setName(user.getName());
+
+        String rawPassword = user.getPassword();
+        String endPassword = bCryptPasswordEncoder.encode(rawPassword);
+
+        userEntity.setPassword(endPassword);
+        userEntity.setBio(user.getBio());
+        userEntity.setWebsite(user.getWebsite());
+        userEntity.setPhone(user.getPhone());
+        userEntity.setGender(user.getGender());
+
+        return userEntity;
+
+    }
+}
--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -1,16 +1,52 @@
 package com.cos.photogramstart.web.api;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.handler.ex.CustomVaildationApiException;
+import com.cos.photogramstart.handler.ex.CustomVaildationException;
+import com.cos.photogramstart.service.UserService;
+import com.cos.photogramstart.web.dto.auth.dto.CMRespDto;
 import com.cos.photogramstart.web.dto.auth.dto.user.UserUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
 @RestController
 public class UserApiController {
 
-    @PutMapping("/api/user/{id}")
-    public String update(UserUpdateDto userUpdateDto) {
-        System.out.println("userUpdateDto = " + userUpdateDto);
-        return "ok";
+    private final UserService userService;
 
+    @PutMapping("/api/user/{id}")
+    public CMRespDto<?> update(
+            @PathVariable int id,
+            @Valid UserUpdateDto userUpdateDto,
+            BindingResult bindingResult,
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+                System.out.println(" = ====================================================================");
+                System.out.println("error.getDefaultMessage() = " + error.getDefaultMessage());
+
+            }
+            throw new CustomVaildationApiException("유효성검사 실패함",errorMap);
+        }else {
+
+            User userEntity = userService.회원수정(id, userUpdateDto.toEntity());
+            principalDetails.setUser(userEntity);
+            return new CMRespDto<>(1, "회원수정완료", userEntity);
+        }
     }
 }
