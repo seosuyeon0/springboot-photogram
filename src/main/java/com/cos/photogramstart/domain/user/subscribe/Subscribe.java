5e2b1b89c839f06ee5dc0136f724ee130ad20c2b
수연 <3437ssy@naver.com>
2023-12-16 17:23:06 +0900

    구독하기 API

---
+++ src/main/java/com/cos/photogramstart/domain/user/subscribe/Subscribe.java
@@ -0,0 +1,46 @@
+package com.cos.photogramstart.domain.user.subscribe;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "subScribe_id",
+                        columnNames = {"fromUserId","toUserId"}
+                )
+        }
+)
+public class Subscribe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "fromUserId")
+   @ManyToOne
+   private User fromUser;
+
+    @JoinColumn(name = "toUserId")
+   @ManyToOne
+   private User toUser;
+
+    private LocalDateTime createDate;
+
+
+    @PrePersist
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+
+    }
+}
